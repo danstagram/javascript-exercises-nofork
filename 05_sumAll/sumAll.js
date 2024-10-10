1cb26384de15bed2d684d62267a8442d9efd167e
Dan <101015168+danstagram@users.noreply.github.com>
2024-10-10 23:26:05 +0200

    Finishes Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
 
+    let variableStart = start;
+    let variableEnd = end;
+
+    //get numbers in correct order
+    if (start > end) {
+        variableStart = end;
+        variableEnd = start;
+    }
+
+    if (start < 0 | end < 0 | typeof start !== "number" | typeof end !== "number" | !Number.isInteger(start) | !Number.isInteger(end)){
+        return "ERROR";
+    }
+
+    let mySum = variableStart;
+
+    for (i = variableStart+1; i <= variableEnd; i++) {
+        mySum = mySum+i;
+    }
+    return mySum;
 };
 
 // Do not edit below this line
